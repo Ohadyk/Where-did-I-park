36d165d48a0c3148dbbd99f9c9eff2a6b31f35b8
Ohadyk <ohadyaakov1@gmail.com>
2023-01-25 20:30:31 +0200

    Init React Native App

---
+++ App.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+const App = () => {
+
+    return(
+        <View style={{display: 'flex', alignItems: 'center'}}>
+            <Text style={{color: 'black', fontSize: 30}}>Init App</Text>
+        </View>
+    )
+}
+
+export default App;
